cf5e1024e9324f755c62f531dcbdedc71f45002d
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-28 18:47:17 +0800

    初步设计上传文件的功能

--- src/components/editor/components/sideBar/sideBarContent/assetsManagement/index.jsx
+++ src/components/editor/components/sideBar/sideBarContent/assetsManagement/index.jsx
@@ -3,11 +3,12 @@ import store from "../../../../store/editorStore";
 import runtime from "../../../../controller/runtime";
 import styles from './assetsManagement.module.scss'
 import axios from "axios";
-import {FolderClose} from "@icon-park/react";
+import {ArrowUp, FolderClose, LeftSmall, Upload, UpSmall} from "@icon-park/react";
 import {IconMap, dirMap} from "./DirMap";
 
 const AssetsManagement = () => {
     const [refAssetsManagement, setRefAssetsManagement] = useState(true);
+    const [showUpload, setShowUpload] = useState(false);
     useEffect(() => {
         store.set('refAssetsManagement', refAssetsManagement);
         store.connect('refAssetsManagement', () => {
@@ -39,6 +40,27 @@ const AssetsManagement = () => {
     const cd = (dir) => {
         runtime.currentDir = runtime.currentDir + `/${dir}`;
         getCurrentDir();
+    }
+
+    const uploadFile = () => {
+        //获得要上传文件的目录
+        const gameName = runtime.currentEditGame;
+        //当前目录。如果当前目录为空，就代表现在是根目录，因此不显示单独的文件，只显示目录
+        const currentDir = runtime.currentDir;
+        const dir = `/${gameName}/game/${currentDir}`;
+        let form = new FormData();
+        form.append('dir', dir);
+        const file = document.getElementById('uploadFile').files[0];
+        let fileName = document.getElementById('uploadFile').value;
+        form.append('file', file, fileName);
+        axios.post(`${runtime.domain}/api/editGame/addAsset/`, form, {headers: {'Content-Type': 'multipart/form-data'}}).then(r => {
+            setShowUpload(false);
+            getCurrentDir();
+        })
+    }
+
+    const switchUpload = () => {
+        setShowUpload(!showUpload);
     }
 
     //开始显示当前目录里的内容
@@ -74,8 +96,24 @@ const AssetsManagement = () => {
         <div>
             素材管理
         </div>
-        <div onClick={back}>
-            返回
+        {
+            runtime.currentDir !== '' && <div className={styles.controlPanel}>
+                <div onClick={back}>
+                    <ArrowUp theme="outline" size="30" fill="#333"/>
+                </div>
+                <div onClick={switchUpload}>
+                    <Upload theme="outline" size="30" fill="#333"/>
+                </div>
+            </div>
+        }
+        {
+            showUpload && <div className={styles.upload}>
+                上传文件<br/>
+                <input id={'uploadFile'} type={'file'}/>
+                <div onClick={uploadFile}>上传</div>
+            </div>
+        }
+        <div>
         </div>
         <div className={styles.dirButtonContainer}>
             {showDirContent}
